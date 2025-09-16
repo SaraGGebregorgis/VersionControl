05225f9507aab4966d2c8e3d684953a85e55d0dd
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-09-16 18:01:43 -0500

    Add banner welcome message at start of program

--- camel.py
+++ camel.py
@@ -3,10 +3,18 @@ def camelcase(sentence):
     upper_camel_cased = title_case.replace('', '')
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    message = 'Awesome camelcase program!!'
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}')
+
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
 
 if __name__ == '__main__':
     main()
+    #commit before making a branch
